1b1bcb566466c16d88bfb3c45e0970d9405ca433
BathriBBN <bbnbadhri@gmail.com>
2023-10-06 12:03:55 +0530

    connect to mongoodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const todoRouter = require('./routes/todos');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -13,6 +14,13 @@ app.use((req, res, next) => {
 
 app.use('/api/todos', todoRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}!!`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`connected to db & listening on port ${process.env.PORT}!!`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
